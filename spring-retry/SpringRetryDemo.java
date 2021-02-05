e7351c53203c159da0a9dc6686b299223da278f6
Danny Hui <39434445+dhui808@users.noreply.github.com>
2021-02-05 16:27:10 -0500

    Create SpringRetryDemo.java

---
+++ spring-retry/SpringRetryDemo.java
@@ -0,0 +1,46 @@
+//Spring-Retry, Hystrix, Async call
+//in Spring Boot Application class, add @EnableRetry and add @EnableCircuitBreaker to a project config class.
+
+public class SpringRetryDemo {
+
+  @Autowired
+  @QUalifier("customerRestTemplate")
+  private RestTemplate restTemplate;
+  
+  @Autowired
+  SpringRetryDemoDelegate springRetryDemoDelegate;
+  
+  @HystrixCommand
+  public Future<Records> customerPost(JsonNode customerInfo, CustomerContext ctx) {
+    return new AsyncResult<Records>() {
+      @Override
+      public Records invoke() {
+        HttpHeaders header = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + ctx.getToken());
+        headers.add("x-client-id", "abc123456");
+        headers.add("Content-Type", "application/x-www-form-urlencoded");
+        headers.add("Accept", "application/json");
+        HttpEntity<JsonNode> requetEntity = new HttpEntity<>(customerInfo, headers);
+        ResponseEntity<List<Records>> response = springRetryDemoDelegate.doCustomerPost(requestEntity);
+        Records records = null;
+        if (null != response && CollectionUtils.isNotEmpty(response.getBody())) {
+          records = response.getBody().get(0);
+        }
+        return records;
+      }
+    };    
+  }
+  
+  @Bean
+  SpringRetryDemoDelegate springRetryDemoDelegate() {
+    return new SpringRetryDemoDelegate();
+  }
+  
+  private class SpringRetryDemoDelegate {
+  
+    @Retrable(maxAttempts = 3, backoff = @Backoff(delay = 1000})
+    public ResponseEntity<List<Records>> doCustomerPost(HttpEntity<JsonNode> request) {
+      response = restTemplate.exchange("/customers", HttpMethod.POST, request, new ParameterizedTypeReference<List<Records>>(){});
+      return response;
+    }
+}
